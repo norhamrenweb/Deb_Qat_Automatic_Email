c32feb9f71ecef6831128b2032b784a80c4045fc
Jesus <Jesus@DESKTOP-ONPGA0B>
2017-05-09 13:41:17 +0200

    Merge origin/master
    Conflicts:
    	web/WEB-INF/dispatcher-servlet.xml

--- src/java/Controllers/CreateSettingControlador.java
+++ src/java/Controllers/CreateSettingControlador.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+import Mails.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.RequestMapping;
